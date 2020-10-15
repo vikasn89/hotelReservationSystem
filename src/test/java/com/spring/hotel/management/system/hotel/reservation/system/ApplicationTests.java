30e749302944d0515cd418ceb58cdf31b03ffa6e
vikasn89 <vikas.n89@gmail.com>
2020-10-15 20:16:26 +0530

    Basic  project structure created

---
+++ src/test/java/com/spring/hotel/management/system/hotel/reservation/system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hotel.management.system.hotel.reservation.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
